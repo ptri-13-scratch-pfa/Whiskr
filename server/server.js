377e07ade14e5bba211554d20a1520e3bcf7d27f
Russ Apolonio <arsenal.dire_0e@icloud.com>
2024-01-11 20:12:31 -0800

    Fixed exposed mongo URI to ENV

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-
+const dotenv = require('dotenv');
+dotenv.config()
 const loginRoute = require("./routes/loginRoutes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.SERV_PORT;
 
 const Profile = require("./models/models.js");
 
 
-const MONGO_URI = 'mongodb+srv://01dukedomlockers:32AGNWM2EbPmJjG1@cluster1.t8kdtqj.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URI = process.env.MONGO_URI;
 
 
 mongoose.connect(MONGO_URI, {
