e3df6a6ba207de4a9f853e73e45a7fe31e526b4f
arthurtcheung <arthurtcheung>
2024-01-20 13:08:32 -0500

    added scrollable info block to cards

--- client/src/pages/CatsCardsPage.js
+++ client/src/pages/CatsCardsPage.js
@@ -42,11 +42,27 @@ const CatDashboard = () => {
             onSwipe={dir => swiped(dir, character.name)}
             onCardLeftScreen={() => outOfFrame(character.name)}
           >
-            <div
-              style={{ backgroundImage: 'url(' + character.imageUrl + ')' }}
-              className='card'
-            >
-              <h3>{character.name}</h3>
+            <div className='card-contents'>
+              <div
+                style={{
+                  backgroundImage: 'url(' + character.imageUrl + ')',
+                  padding: '10px',
+                  boxSizing: 'border-box',
+                }}
+                className='card'
+              >
+                <h2 className='card-name'>
+                  {character.name + ', ' + character.age}
+                </h2>
+              </div>
+              <div className='more-info-block'>
+                <label>breed</label>
+                <p className='card-fetched-data'>{character.breed}</p>
+                <label>contact</label>
+                <p className='card-fetched-data'>{character.email}</p>
+                <label>about me</label>
+                <p className='card-fetched-data'>{character.aboutMe}</p>
+              </div>
             </div>
           </TinderCard>
         ))}
