dc03cd36fbbf62b1321a460079ee63738107e39d
arthurtcheung <arthurtcheung>
2024-01-24 16:16:03 -0500

    cleaned out unnecessary modules from webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-const path = require('path');
-const dotenv = require('dotenv').config();
-const DotenvWebpack = require('dotenv-webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// Modules
+const path = require("path");
+const dotenv = require("dotenv");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+dotenv.config();
 
 module.exports = {
-  entry: path.join(__dirname, './client/src/', 'index.js'),
+  entry: path.join(__dirname, "./client/src/", "index.js"),
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, "build"),
   },
   mode: process.env.MODE,
   module: {
@@ -15,49 +17,48 @@ module.exports = {
         test: /\.(?:js|mjs|cjs|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             presets: [
-              ['@babel/preset-env', { targets: 'defaults' }],
-              '@babel/preset-react',
+              ["@babel/preset-env", { targets: "defaults" }],
+              "@babel/preset-react",
             ],
           },
         },
       },
       {
         test: /\.(css|scss)$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|jpe?g|JPG)$/,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   plugins: [
-    new DotenvWebpack(),
     new HtmlWebpackPlugin({
-      title: 'Whiskr',
-      template: path.join(__dirname, './client/public', 'index.html'),
+      title: "Whiskr",
+      template: path.join(__dirname, "./client/public", "index.html"),
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, '/build'),
-      publicPath: '/',
+      directory: path.join(__dirname, "/build"),
+      publicPath: "/",
     },
     hot: true,
     historyApiFallback: true,
     compress: true,
     port: process.env.DEV_PORT,
     proxy: {
-      '/': {
-        target: `http://localhost:3000`,
+      "/": {
+        target: `http://localhost:${process.env.SERV_PORT}`,
       },
     },
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
   performance: {
     hints: false,
